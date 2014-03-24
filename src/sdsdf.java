832b9505a1d95a9b276f4d1bb4e3cab25d55a5e2
rivetech <rivetech@85.23.67.137>
2014-03-24 18:35:38 +0200

    kolmas printti

--- src/sdsdf.java
+++ src/sdsdf.java
@@ -14,6 +14,7 @@ public class sdsdf {
         System.out.println("kaara");
          System.out.println("kaara");
          System.out.println("kaara");
+          System.out.println("kaara2222222");
     }
     
 }
