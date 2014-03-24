aa425d2b0e56e83aa135664f34b2e461e2aeed4f
rivetech <rivetech@85.23.67.137>
2014-03-24 18:41:16 +0200

    4s printti

--- src/sdsdf.java
+++ src/sdsdf.java
@@ -14,7 +14,8 @@ public class sdsdf {
         System.out.println("kaara");
          System.out.println("kaara");
          System.out.println("kaara");
-          System.out.println("kaara2222222");
+          System.out.println("kaara2222222");   
+       System.out.println("ooota - joo");   
     }
     
 }
