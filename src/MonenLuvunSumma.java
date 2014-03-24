e9f5dfdd9c6c21b83ea249d16e727b24476590b8
rivetech <rivetech@85.23.67.137>
2014-03-24 23:42:15 +0200

    VitustiTavaraa

---
+++ src/MonenLuvunSumma.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Harjoitukset_MOOC_Viikko2;
+
+/**
+ *
+ * @author rivetech
+ */
+public class MonenLuvunSumma {
+    public static void main(String[] args) {
+        
+        int eka = 3;
+        int toka = 2;
+        
+        int = monenLuvunSumma = summa(summa(1,2), summa(eka, toka));
+        
+        System.out.println("Summa: " + summa);        
+        
+    }
+    public static int summa(int monenLuvunSumma) {
+    return monenLuvunSumma;
+}
+    
+}
